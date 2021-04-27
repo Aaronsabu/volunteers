1e50276d4ddcdd1961b4673537feea626a3261cf
AUGUSTIN-SHAJAN <71377446+AUGUSTIN-SHAJAN@users.noreply.github.com>
2021-04-27 12:02:27 +0530

    Add files via upload

---
+++ src/Components/ButtonA.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Text,View,StyleSheet,Pressable} from 'react-native'
+
+const ButtonA=({title,onPress,style})=>{
+    let {height,width,marginTop,marginBottom,backgroundColor,fontSize,borderRadius}=style 
+console.log(style)
+    return (
+    <Pressable onPress={onPress}>
+
+    <View style={{
+    backgroundColor: backgroundColor,
+    height:height,
+    width:width,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop:marginTop,
+    marginBottom:marginBottom,
+    borderRadius: borderRadius
+}}>
+    <Text style={{
+    fontSize: fontSize,
+    alignSelf: "center",
+    color: "white"
+}}>
+{title}
+</Text>
+    </View>
+
+    </Pressable>)
+
+}
+
+const styles=StyleSheet.create({})
+export default ButtonA
