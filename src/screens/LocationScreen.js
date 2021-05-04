a5e4feaf877309c0ec5f8300c06659004a00d756
Aaronsabu <70811901+Aaronsabu@users.noreply.github.com>
2021-05-04 17:40:39 +0530

    Update LocationScreen.js

--- src/screens/LocationScreen.js
+++ src/screens/LocationScreen.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet, Image} from 'react-native';
+import Call from '../components/Call';
 
 const LocationScreen = () => {
-    return <View>
-        <Text>Location Screen</Text>
-    </View>
+    return (
+       <View style={styles.container}>
+         <Image style={styles.img} source={require('../img/pinkpal.png')} /> 
+         <Call />
+       </View>
+    );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        marginTop:5,
+        alignItems: 'center',
+        
+    },
+    img: {
+        height: 70,
+        width: 100
+    }
+});
 
 export default LocationScreen;
