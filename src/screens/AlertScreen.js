e286222c8ebe8057243a015b4dbe6dfbe0c258a5
Aaronsabu <70811901+Aaronsabu@users.noreply.github.com>
2021-05-16 11:48:57 +0530

    Update AlertScreen.js

--- src/screens/AlertScreen.js
+++ src/screens/AlertScreen.js
@@ -24,7 +24,7 @@ const AlertScreen = ({ navigation}) => {
         <ButtonA 
         style={styles.Button}
         title="Not Available" 
-        onPress={() => navigation.navigate('Location')}/>
+        onPress={() => navigation.navigate('Default')}/>
 
         </View>
 };
