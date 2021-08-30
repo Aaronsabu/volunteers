35e32d76d3adf76bd12f86f464b319497f6e2d85
Aaronsabu <70811901+Aaronsabu@users.noreply.github.com>
2021-08-30 10:37:52 +0530

    Update LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,22 +1,47 @@
-import React, {useState} from 'react';
-import { StatusBar, View, Text, Image, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Image, StyleSheet, Platform } from 'react-native';
 import {Input, Button} from 'react-native-elements';
+import { auth } from '../firebase/config';
 import NavLink from '../components/NavLinks';
+import { navigate } from '../navigationRef';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
-const LoginScreen = (props) => {
+const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        navigate("Default");
+        console.log("Success");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const signIn = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error));
+  };
+  
+
   return (
+    <KeyboardAwareScrollView
+    resetScrollToCoords={{ x: 0, y: 0 }}
+    contentContainerStyle={styles.container}
+    scrollEnabled={false}
+     >
     <View style={styles.container}>
-      <StatusBar style="light" />
       <Image
         source={require('../img/pinkpal.png')}
         style={{width: 300, height: 150}}
       />
       <View style={styles.inputContainer}>
         <Input
-          label="Email"
+          placeholder="Email"
           type="email"
           value={email}
           onChangeText={text => { setEmail(text) }}
@@ -24,23 +49,31 @@ const LoginScreen = (props) => {
           autoCorrect={false}
         />
         <Input
-          label="Password"
+          placeholder="Password"
           secureTextEntry
           type="password"
           value={password}
           onChangeText={password => { setPassword(password) }}
+          onSubmitEditing={signIn}
           autoCapitalize="none"
           autoCorrect={false}
         />
       </View>
-      <Button containerStyle={styles.button} title="Login" />
+      <Button containerStyle={styles.button} title="Login" onPress={signIn} />
 
       <NavLink 
-        text="Don't have an account? Sign up instead"
+        text="Don't have an account? Sign up instead!"
         routeName="Register"
       />
     </View>
+    </KeyboardAwareScrollView>
   );
+};
+
+LoginScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
 };
 
 const styles = StyleSheet.create({
@@ -48,11 +81,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 10,
+    paddingTop: Platform.OS === 'android' ? 25 : 0
   },
   button: {
     width: 200,
     marginTop: 30,
+    marginBottom:10
   },
   inputContainer: {
     width: 300,
