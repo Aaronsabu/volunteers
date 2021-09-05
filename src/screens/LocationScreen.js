727b8cf8025233008e63512fd670f75ba40ab153
Akhilbenny <63584404+Akhilbenny@users.noreply.github.com>
2021-09-05 20:08:32 +0530

    Update LocationScreen.js

--- src/screens/LocationScreen.js
+++ src/screens/LocationScreen.js
@@ -1,9 +1,11 @@
 //import '../_MockLocation';
 import React, {useEffect} from 'react';
-import {Text, View, StyleSheet, Image,SafeAreaView} from 'react-native';
+import { View, StyleSheet, Image} from 'react-native';
 import Call from '../components/Call';
 import Tracker from '../components/Tracker';
 import Trackee from '../components/Trackee';
+import { ScrollView } from 'react-native-gesture-handler';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 //import useLocation from '../hooks/useLocation';
 
 const LocationScreen = () => {
@@ -11,7 +13,7 @@ const LocationScreen = () => {
 
 
     return (
-        
+        //<ScrollView>
        <View style={styles.container}>
          <Image style={styles.img} source={require('../img/pinkpal.png')} /> 
           
@@ -19,22 +21,28 @@ const LocationScreen = () => {
          <Call />
          <Trackee />
        </View>
+       //</ScrollView>
        
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        marginTop:4,
-        marginBottom:20,
-        alignItems: 'center',
+        //flex:1,
+
+        height: hp('85%'), // 70% of height device screen
+  // width: wp('95%') ,  // 80% of width device screen
+       //marginTop:4,
+       marginBottom:20,
+      alignItems: 'center',
         
     },
     img: {
-        
-        //marginTop:5,
-        height: 70,
+     // justifyContent: 'center',
+    // alignItems: 'center',
+      // flex:1,
+      //  marginleft:50,
+       height: 70,
         width: 100
     }
 });
