4bc41f4f78df8e5875f19a8e2b7d218c9e31f345
Aaronsabu <70811901+Aaronsabu@users.noreply.github.com>
2021-09-22 11:40:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
 import DefaultScreen from './src/screens/DefaultScreen';
 import AlertScreen from './src/screens/AlertScreen';
 import LocationScreen from './src/screens/LocationScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import { setNavigator } from './src/navigationRef';
+import LoadingScreen from './src/screens/loading';
 
-const navigator = createStackNavigator({
-  Default: DefaultScreen,
-  Alert: AlertScreen,
-  Location: LocationScreen,
-}, {
-  initialRouteName: 'Default',
-  defaultNavigationOptions: {
-    title: 'Volunteers'
-  }
+const switchNavigator = createSwitchNavigator({
+  Loading: LoadingScreen,
+  loginFlow: createStackNavigator({
+    Login: LoginScreen,
+    Register: RegisterScreen
+  }),
+  mainFlow: createStackNavigator({
+    Default: DefaultScreen,
+    Alert: AlertScreen,
+    Location: LocationScreen
+  }),
 });
 
-const App = createAppContainer(navigator);
+
+const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
-    <App />
+      <App ref={(navigator) => { setNavigator(navigator) }} /> 
   );
 };
+
