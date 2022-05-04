c72f09d9a36da021554687fd3792d28a8c50264c
Aaronsabu <70811901+Aaronsabu@users.noreply.github.com>
2022-05-04 08:06:02 +0530

    Create LocationScreen.js

--- src/screens/LocationScreen.js
+++ src/screens/LocationScreen.js
@@ -1,42 +1,69 @@
 //import '../_MockLocation';
-import React, {useEffect} from 'react';
-import {Text, View, StyleSheet, Image} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, Image, RefreshControl, ScrollView} from 'react-native';
 import Call from '../components/Call';
 import Tracker from '../components/Tracker';
 import Trackee from '../components/Trackee';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 //import useLocation from '../hooks/useLocation';
 
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  }
+
 const LocationScreen = () => {
-    //const [errorMsg] = useLocation(isFocused);
+
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(2000).then(() => setRefreshing(false));
+      }, []);
 
 
     return (
        <View style={styles.container}>
+         <ScrollView
+           contentContainerStyle={styles.scrollView}
+           refreshControl={
+           <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+            />
+           }
+         >
          <Image style={styles.img} source={require('../img/pinkpal.png')} /> 
           
          <Tracker />
          <Call />
          <Trackee />
+         </ScrollView>
        </View>
     );
 };
 
-LocationScreen.navigationOptions = () => {
-    return {
-      headerShown: false,
-    };
-  };
+// LocationScreen.navigationOptions = () => {
+//     return {
+//       headerShown: false,
+//     };
+//   };
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop:20,
         flex: 1,
+        height: hp('85%'),
+        marginBottom:20,
         alignItems: 'center',
         
     },
+    scrollView: {
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
     img: {
         height: 70,
-        width: 100
+        width: 100,
+        alignItems: 'center'
     }
 });
 
